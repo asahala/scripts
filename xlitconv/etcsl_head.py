24030be3296b763129c565eabeac835e18108a83
asahala <aleksi.sahala@helsinki.fi>
2016-09-11 01:48:44 +0300

    First commit

---
+++ xlitconv/etcsl_head.py
@@ -0,0 +1,29 @@
+head = '''
+<head><title>The Electronic Text Corpus of Sumerian</title>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+<link rel='stylesheet' href='http://etcsl.orinst.ox.ac.uk/edition2/css/etcslsearch.css'/>
+<link rel='shortcut icon' href='http://etcsl.orinst.ox.ac.uk/edition2/gif/igi.ico'/>
+<script src='http://etcsl.orinst.ox.ac.uk/edition2/js/etcsl.js' type='text/javascript'></script>
+<script src='http://etcsl.orinst.ox.ac.uk/edition2/js/ttstart.js' type='text/javascript'></script>
+<script>
+/*************************************************************************
+ dw_event.js (version date Feb 2004)
+ dw_viewport.js  version date Nov 2003
+ dw_tooltip.js
+ version date: Nov 2003
+ requires: dw_event.js and dw_viewport.js
+        
+    This code is from Dynamic Web Coding at http://www.dyn-web.com/
+    See Terms of Use at http://www.dyn-web.com/bus/terms.html
+    regarding conditions under which you may use this code.
+    This notice must be retained in the code as is!
+*************************************************************************/
+</script>
+</head>
+'''
+
+scripts = '''
+<script src='http://etcsl.orinst.ox.ac.uk/edition2/js/dw_event.js' type='text/javascript'></script>
+<script src='http://etcsl.orinst.ox.ac.uk/edition2/js/dw_viewport.js' type='text/javascript'></script>
+<script src='http://etcsl.orinst.ox.ac.uk/edition2/js/dw_tooltip.js' type='text/javascript'></script>
+'''
